0d29272f21865b72925048aedb2d88bf934617a9
Tomas Juhaniak <juhaniak.tomas@gmail.com>
2020-07-20 01:19:41 +0200

    Add simple regex in memory search.

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FeedsService } from 'src/app/services/feeds.service';
 import { HooksWatcher } from 'src/app/models/hooks-watcher';
 import { Feed } from 'src/app/models/feeds';
+import { FormControl } from '@angular/forms';
+
+const escape: RegExp = new RegExp(/[.*+?^${}()|[\]\\]/g);
 
 @Component({
   selector: 'app-list',
@@ -11,6 +14,8 @@ import { Feed } from 'src/app/models/feeds';
 export class ListComponent extends HooksWatcher implements OnInit {
 
   public displayedFeeds: Feed[] = [];
+  private allFeeds: Feed[] = [];
+  public filter: FormControl = new FormControl('');
 
   constructor(private feeds: FeedsService) {
     super();
@@ -20,8 +25,20 @@ export class ListComponent extends HooksWatcher implements OnInit {
     this.feeds.getFeeds()
       .pipe(this.takeUntilDestroyed())
       .subscribe(feeds => {
-        this.displayedFeeds = feeds;
+        this.allFeeds = feeds;
+        this.applyFilter();
       });
+    
+    this.filter.valueChanges
+      .pipe(this.takeUntilDestroyed())
+      .subscribe(() => {
+        this.applyFilter();
+      });
+  }
+
+  private applyFilter(): void {
+    const regFilter = new RegExp('.*' + (this.filter.value as string).toLocaleLowerCase().split('').map(s => s.replace(escape, '\\$&')).join('.{0,3}') + '.*');
+    this.displayedFeeds = this.allFeeds.filter(f => regFilter.test((f.name + f.text).toLocaleLowerCase()));
   }
 
 }
